6076166f795944fc188cb9959dfe02e2a83d336b
Anfas Pervez <anfaspervez99@gmail.com>
2020-10-20 00:39:15 +0500

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
+import Header from './components/header';
+import Recipes from './components/recipes';
+import Axios from 'axios';
+
 
 function App() {
+
+  const [search,setSearch]=useState("chicken");
+  const [recipes,setRecipes]=useState([]);
+
+  const APP_ID="9736d5cc";
+  const APP_KEY="8941caac7e5b2cab4e57dc2ac2cd4012";
+ 
+ const getRecipes= async ()=>{
+    const res= await Axios.get(`https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`);
+    setRecipes(res.data.hits);
+  }
+  useEffect(()=>{
+     getRecipes();
+   }, []);
+
+  
+
+  const onInputChange= e =>{
+    setSearch(e.target.value);
+  }
+
+  const onSearchClick=()=>{
+    getRecipes();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header search={search}  onSearchClick={onSearchClick} onInputChange={onInputChange}/>
+      <div className="container">
+        <Recipes recipes={recipes}/>
+      </div>
     </div>
   );
 }
